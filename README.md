761af6375d7a111f56ecd133114382edb8b02575
Christopher Figueroa <chrisfigueroa16@gmail.com>
2017-01-07 12:27:37 -0800

    started install command

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Poly - Coming Soon
 ## A Engine Agnostic Game Package Manager
 
+
+[![Build Status](https://travis-ci.org/kinifi/poly-client.svg?branch=master)](https://travis-ci.org/kinifi/poly-client)
+[![Dependencies](https://david-dm.org/kinifi/poly-client.svg)](https://david-dm.org/kinifi/poly-client.svg)
+
 ![Game Package Manager](http://i.imgur.com/dU0w44o.png)
 
 #### A Package Manager for Game Developers. Have you ever needed a small script or plugin and had to go find it? Poly makes all this easier.
@@ -8,7 +12,7 @@
 
 ---
 
-##What is this? 
+##What is this?
 
 This is a package manager made for game devs. Most game devs just want to easily pull down plugins and put them in their desired location. NPM makes it convenient by putting everything in a "nodemodules" folder but they aren't writing games for 6+ SKU's on different hardware. We need to easily pull down plugins, scripts, and placeholder art and put them in a location *we choose*.
 
@@ -16,13 +20,13 @@ This is a package manager made for game devs. Most game devs just want to easily
 
 ##How does this work?
 
-The first implementation will store gpm.json files into a database the contains a bunch of information but more importantly Git Repo links. When you type commands like 
+The first implementation will store gpm.json files into a database the contains a bunch of information but more importantly Git Repo links. When you type commands like
 
 ```
 gpm install steamworksdotnet
 ```
 
-The assets for that git repo are stored on github, bitbucket, or whatever else. Soon to be on our own servers. 
+The assets for that git repo are stored on github, bitbucket, or whatever else. Soon to be on our own servers.
 
 ---
 
@@ -36,4 +40,4 @@ Poly - a package that contains assets to be consumed in a game project.
 
 You can install *polys* locally to a general GPM directory on your computer with the ```gpm install steamworksdotnet -g``` command. This will save that *poly* for later use. So incase you aren't connected to the internet or have slow internet you can call ```gpm install steamworksdotnet -l```
 
-
+---
--- app.js
+++ app.js
@@ -1,13 +1,75 @@
-var prompt = require('prompt');
-var fs = require('fs');
-var colors = require('colors');
+#!/usr/bin/env node --harmony
+
+var prompt      = require('prompt');
+var fs          = require('fs');
+var colors      = require('colors');
 var progressBar = require('progress');
-var requestify = require('requestify');
+var program     = require('commander');
+var path        = require('path');
+var superagent  = require('superagent');
+var simplegit   = require('simple-git');
+var path        = require('path');
+var process     = require('process');
+var dirString   = path.resolve(process.cwd());
 
-//example of command call
-//node app.js install steamworksdotnet -l
+//the server to pull polys from. If you are hosting on your own, change this to your server IP
+var serverLocation = "http://localhost:9000";
+//console.log('working directory', dirString);
 
-for (var i = 0; i < process.argv.length; i++) {
-  //ignore the first and second index
-  console.log(i + ": " + process.argv[i]);
-}
+program
+  .version('0.0.1')
+
+program
+  .command('install [polyname]')
+  .description('installs poly on the working directory')
+  .option("-g, --global", "Installs to a local cache folder for later use instead of pulling from the web")
+  .action(function(polyname, options) {
+
+    //get the for the poly from the server
+    superagent
+      .get(serverLocation + '/api/poly/' + polyname)
+      .end(function(err, res){
+        //check if error
+        if(err){
+          console.log(err);
+        }
+
+        if(res)
+        {
+          //get the repo url
+          var repourl = res.body[0].repourl;
+          console.log(repourl);
+          //
+          // simplegit()
+          //  .outputHandler(function (command, stdout, stderr) {
+          //     stdout.pipe(process.stdout);
+          //     stderr.pipe(process.stderr);
+          //  })
+          //  //clone into the directory we are in right now
+          //  .clone('https://' + repourl, dirString);
+        }
+
+      });
+
+    if(options.global) {
+        var config = options.global;
+        console.log("Install Globally: " + config);
+    }
+    else {
+
+    }
+
+
+    console.log('installing ' + polyname);
+  });
+
+
+program
+  .command('*')
+  .description('error catching')
+  .action(function(env){
+    console.log('not a command "%s"', env);
+  });
+
+
+program.parse(process.argv);
